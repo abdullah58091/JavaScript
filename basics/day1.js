615b9fe192b57a01c27eb8c70edaffffb4953712
abdullah58091 <abdullahjd6@gmail.com>
2026-05-02 00:20:14 +0530

    Day 1: JS basics setup

---
+++ basics/day1.js
@@ -0,0 +1,15 @@
+// Day 1: Variables & Data Types
+
+// String
+let name = "Abdullah";
+
+// Number
+const age = 20;
+
+// Boolean
+var isStudent = true;
+
+// Output
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
